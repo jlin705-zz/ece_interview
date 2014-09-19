77a8733982ed9aafd07530a04fec94d8525946de
Jialiang Lin <jlin705@gmail.com>
2014-09-18 23:20:07 -0400

    centered table

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  #resources :questions
+  resources :questions
 
   resources :companies do
     resources :questions
   end
 
-  
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
