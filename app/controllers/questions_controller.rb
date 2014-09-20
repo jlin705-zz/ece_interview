039270bd51d2605727d6b008b9ce734425bd32e2
Jialiang Lin <jlin705@gmail.com>
2014-09-19 23:07:03 -0400

    with author in questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -72,6 +72,6 @@ class QuestionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def question_params
-      params.require(:question).permit(:company_id, :question)
+      params.require(:question).permit(:company_id, :question, :author)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  def self.current
+    Thread.current[:user]
+  end
+  def self.current=(user)
+    Thread.current[:user] = user
+  end
 end
