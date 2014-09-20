64a7a58a02a43e77e3cf9e8bcd1fe77c2d011464
Jialiang Lin <jlin705@gmail.com>
2014-09-19 21:24:37 -0400

    Authentication added

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,6 +1,7 @@
 class CompaniesController < ApplicationController
   before_action :set_company, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate, except: [:index, :show]
+  # before_action :authenticate, except: [:index, :show]
+  before_action :authenticate_user!, :except => [:index]
 
   # GET /companies
   # GET /companies.json
